acdb257be7dec96aa3804536d285806b6157bfe2
Pond <6410685033@student.tu.ac.th>
2024-04-10 15:31:06 +0700

    create chat view and viewModel

---
+++ DermAssist/DermAssist/ViewModels/Chat/ChatViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ChatViewModel.swift
+//  DermAssist
+//
+//  Created by Thammasat Thonggamgaew on 10/4/2567 BE.
+//
+
+import Foundation
+
---
+++ DermAssist/DermAssist/Views/Chat/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  DermAssist
+//
+//  Created by Thammasat Thonggamgaew on 10/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
