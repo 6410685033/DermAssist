b452689e12a96dcb1c7834ea0a5085028a2e78f7
Pond <6410685033@student.tu.ac.th>
2024-04-10 13:47:54 +0700

    start project

---
+++ DermAssist/DermAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DermAssist
+//
+//  Created by Thammasat Thonggamgaew on 10/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DermAssist/DermAssist/DermAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DermAssistApp.swift
+//  DermAssist
+//
+//  Created by Thammasat Thonggamgaew on 10/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DermAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
