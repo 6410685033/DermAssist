0c8ec65ffa84682d81e9d9f62a7d1a56ade0c45f
Pond <6410685033@student.tu.ac.th>
2024-04-16 22:50:07 +0700

    arrange ProfileView about my allergen

---
+++ DermAssist/DermAssist/Views/Login/AllergenView.swift
@@ -0,0 +1,48 @@
+//
+//  AllergenView.swift
+//  DermAssist
+//
+//  Created by Thammasat Thonggamgaew on 16/4/2567 BE.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct AllergenView: View {
+    @ObservedObject var viewModel: ProfileViewModel
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                    let columns: [GridItem] = [
+                        GridItem(.flexible()),
+                        GridItem(.flexible())
+                    ]
+                    
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 10) {
+                            ForEach(viewModel.myAllergens, id: \.id) { allergen in
+                                Text(allergen.allergen_name)
+                                    .padding()
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                    .background(Color.secondary.opacity(0.1))
+                                    .cornerRadius(5)
+                            }
+                        }
+                        .padding()
+                    }
+            }
+            .navigationBarTitle("My Allergens", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Done") {
+                isPresented = false
+            })
+        }
+    }
+}
+
+
+#Preview {
+    AllergenView(viewModel: ProfileViewModel(), isPresented: .constant(true))
+}
--- DermAssist/DermAssist/Views/Login/ProfileView.swift
+++ DermAssist/DermAssist/Views/Login/ProfileView.swift
@@ -10,99 +10,106 @@ import SwiftUI
 struct ProfileView: View {
     @StateObject var viewModel = ProfileViewModel()
     @State private var showingEditPage = false
-    @State private var showCreateNewAllergen = false
+    @State private var showingAllergenPage = false  // State for showing AllergenView
     @State private var showConfirmationAlert = false
     @State private var showAlert = false
     @State private var alertMessage = ""
-
+    
     var body: some View {
         NavigationView {
             VStack {
                 if let user = viewModel.user {
-                    ScrollView {
-                        VStack(spacing: 20) {
-                            Image(systemName: "person.circle.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 200)
-
-                            Text(user.name)
-                                .font(.title)
-                                .bold()
-
-                            Text(user.email)
-                                .foregroundColor(.secondary)
-
-                            Text(user.tel)
-                                .foregroundColor(.secondary)
-
-                            if viewModel.user!.role.isDoctor {
-                                Button(action: {
-                                    showCreateNewAllergen = true
-                                }) {
-                                    Label("Add Allergen", systemImage: "plus.circle.fill")
-                                        .foregroundColor(.green)
-                                }
-                            }
-
-                            VStack(spacing: 10) {
-                                ForEach(viewModel.myAllergens, id: \.id) { allergen in
-                                    Text(allergen.allergen_name)
-                                }
-                            }
-
-                            Button("Delete Account") {
-                                showConfirmationAlert = true
-                            }
-                            .foregroundColor(.red)
-                            .alert("Are you sure you want to delete your account?", isPresented: $showConfirmationAlert) {
-                                Button("Cancel", role: .cancel) {}
-                                Button("Delete", role: .destructive) {
-                                    viewModel.deleteAccount { success, error in
-                                        if success {
-                                            print("Account deleted")
-                                        } else {
-                                            alertMessage = error?.localizedDescription ?? "An error occurred"
-                                            showAlert = true
-                                        }
-                                    }
-                                }
-                            } message: {
-                                Text("This action cannot be undone.")
-                            }
-                            .alert("Error", isPresented: $showAlert) {
-                                Button("OK", role: .cancel) {}
-                            } message: {
-                                Text(alertMessage)
-                            }
-
-                            Button("Log Out") {
-                                viewModel.logOut()
-                            }
-                            .padding()
-                            .frame(width: 220, height: 50)
-                            .background(Color.gray)
-                            .foregroundColor(.white)
-                            .clipShape(Capsule())
+                    VStack(spacing: 20) {
+                        profileImage
+                        
+                        labeledText("Name", value: user.name)
+                        labeledText("Email", value: user.email)
+                        labeledText("Telephone", value: user.tel)
+                        labeledText("Gender", value: user.gender)
+                        
+                        Button("My Allergens") {
+                            showingAllergenPage = true
                         }
                         .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                        
+                        Spacer()
+                        
+                        deleteAccountButton
+                        logOutButton
                     }
-                    .navigationBarItems(trailing: Button(action: {
-                        showingEditPage = true
-                    }) {
-                        Image(systemName: "gearshape.fill")
-                    })
+                    .padding()
+                    .navigationBarItems(trailing: editProfileButton)
+                    .sheet(isPresented: $showingAllergenPage) {
+                        AllergenView(viewModel: viewModel, isPresented: $showingAllergenPage)
+                    }
                 } else {
-                    Text("Loading Profile")
+                    Text("Loading Profile...")
                 }
             }
             .navigationTitle("Profile")
             .fullScreenCover(isPresented: $showingEditPage, onDismiss: viewModel.fetchUser) {
                 EditProfileView(viewModel: viewModel)
             }
-            .sheet(isPresented: $showCreateNewAllergen) {
-                CreateNewAllergenView(CreateNewAllergenPresented: $showCreateNewAllergen)
+        }
+    }
+    
+    private var profileImage: some View {
+        Image(systemName: "person.circle.fill")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+//            .padding(.top, 20)
+    }
+    
+    private func labeledText(_ label: String, value: String) -> some View {
+        HStack {
+            Text("\(label):")
+                .bold()
+            Text(value)
+            Spacer()
+        }
+    }
+    
+    
+    private var deleteAccountButton: some View {
+        Button("Delete Account") {
+            showConfirmationAlert = true
+        }
+        .foregroundColor(.red)
+        .alert("Are you sure you want to delete your account?", isPresented: $showConfirmationAlert) {
+            Button("Cancel", role: .cancel) {}
+            Button("Delete", role: .destructive) {
+                viewModel.deleteAccount { success, error in
+                    if success {
+                        print("Account deleted")
+                    } else {
+                        alertMessage = error?.localizedDescription ?? "An error occurred"
+                        showAlert = true
+                    }
+                }
             }
+        } message: {
+            Text("This action cannot be undone.")
+        }
+    }
+    
+    private var logOutButton: some View {
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .padding()
+        .frame(width: 220, height: 50)
+        .background(Color.gray)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+    }
+    
+    private var editProfileButton: some View {
+        Button(action: { showingEditPage = true }) {
+            Image(systemName: "pencil")
         }
     }
 }
